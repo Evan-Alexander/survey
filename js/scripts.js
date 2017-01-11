ecca080c2dba16754effe8323c6ab2876ac543e5
Jason Brown <jasontbrown99@gmail.com>
2017-01-11 14:30:50 -0800

    add js for each form provided on epicodus sheet

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// js for dropdown
+
+var beverage = $("#beverage").val();
+
+
+// js for radio
+
+var flavor = $("input:radio[name=flavor]:checked").val();
+
+// js for calendar
+
+var dob = $("#born").val();
+
+// js for color picker
+
+var favoriteColor = $("#color").val();
