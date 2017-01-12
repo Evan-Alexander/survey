a24b1365629e63b431a085997bb46ffb8b6d5363
Jason Brown <jasontbrown99@gmail.com>
2017-01-11 17:15:20 -0800

    set up confirmation form

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 // js for dropdown
 $(document).ready(function() {
   $("form#troubles").submit(function(event) {
-    var troubles = parseInt($("input#troubles").val());
     event.preventDefault();
+    var troubles = parseInt($("input#troubles").val());
+    $("")
   });
 });
 
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
 
 
 // js for radio
@@ -19,3 +29,20 @@ var date = $("#born").val();
 // js for color picker
 
 var favoriteColor = $("#color").val();
+
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var phoneInput = parseInt($("input#phone").val());
+    var emailInput = parseInt($("input#email").val());
+
+    $(".name").text(nameInput);
+    $(".phone").text(phoneInput);
+    $(".email").text(emailInput);
+
+
+    $("#receipt").show();
+
+    event.preventDefault();
+  });
+});
