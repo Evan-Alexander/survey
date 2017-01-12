2425b2ad7e7dca9a72f6d960f7acf8c1d8a99473
Jason Brown <jasontbrown99@gmail.com>
2017-01-11 17:22:57 -0800

    got rid of extraneous js.  time to start over

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,7 @@
 // js for dropdown
-$(document).ready(function() {
-  $("form#troubles").submit(function(event) {
-    event.preventDefault();
-    var troubles = parseInt($("input#troubles").val());
-    $("")
-  });
-});
 
-$(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-});
+var troubles = $("#troubles").val();
+
 
 
 // js for radio
@@ -29,20 +15,3 @@ var date = $("#born").val();
 // js for color picker
 
 var favoriteColor = $("#color").val();
-
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var phoneInput = parseInt($("input#phone").val());
-    var emailInput = parseInt($("input#email").val());
-
-    $(".name").text(nameInput);
-    $(".phone").text(phoneInput);
-    $(".email").text(emailInput);
-
-
-    $("#receipt").show();
-
-    event.preventDefault();
-  });
-});
